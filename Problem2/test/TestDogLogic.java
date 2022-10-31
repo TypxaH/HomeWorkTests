86d3669c7ec7ea0fe8516be5d79aa19a62459c64
TypxaH <turhan@abv.bg>
2022-10-31 10:48:09 +0200

    Same changes

--- Problem2/test/TestDogLogic.java
+++ Problem2/test/TestDogLogic.java
@@ -5,14 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestDogLogic {
     @Test
     public void testMakeSound() {
-        Dog dog = new Dog("Bulonka","Shugar",5,2.5);
+        Dog dog = new Dog("Bolonka","Shugar",5,2.5);
         String actualSound = dog.makeSound();
         String expectedSound = "Bau Bau!";
         assertEquals(expectedSound,actualSound);
     }
     @Test
     public void testEat1() {
-        Dog dog = new Dog("Bulonka","Shugar",5,2.5);
+        Dog dog = new Dog("Bolonka","Shugar",5,2.5);
         dog.eat("food");
         double actualWeight = dog.getWeightKg();
         double expectedWeight = 2.55;
@@ -20,7 +20,7 @@ public class TestDogLogic {
     }
     @Test
     public void testEat2() {
-        Dog dog = new Dog("Bulonka","Shugar",5,10);
+        Dog dog = new Dog("Bolonka","Shugar",5,10);
         dog.eat("food");
         double actualWeight = dog.getWeightKg();
         double expectedWeight = 10.05;
